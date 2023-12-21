0111c4cd8f5eeb35d023766d1021506d35e51bb8
Ali Jahangiri <75624145+aliphys@users.noreply.github.com>
2023-12-21 15:28:00 +0100

    asd

--- examples/AdvancedUSBInternalOperations/AdvancedUSBInternalOperations.ino
+++ examples/AdvancedUSBInternalOperations/AdvancedUSBInternalOperations.ino
@@ -4,15 +4,15 @@
     Demonstrates advanced usage of the "Arduino_UnifiedStorage" library with USB & internal storage, including file operations.
     Creates, copies, and moves files between storage types, and prints folder contents.
 
-    In the setup function, the code initializes serial communication, mounts both USB & internal storage and 
+    In the setup function, the code initializes serial communication, mounts both USB & internal storage and
     reformats the internal storage for a clean file system. Then, it creates a root directory in the internal storage
     and creates a subdirectory with a file inside it containing the string "Hello World!".
 
     Then, it copies the file from internal storage to USB storage and moves the subdirectory from internal storage to USB storage.
 
-    After the file operations, the code prints the contents of both the USB storage and the internal storage. 
+    After the file operations, the code prints the contents of both the USB storage and the internal storage.
     It recursively prints the directories (marked as "[D]") and files (marked as "[F]") using the "printFolderContents" function.
-    
+
     Created 28th July 2023
     By Cristian Dragomir
 
@@ -28,7 +28,6 @@
 USBStorage usbStorage;
 InternalStorage internalStorage;
 
-
 // Helper function to prints the contents of a folder, including subdirectories (marked as "[D]") and files (marked as "[F]").
 void printFolderContents(Folder dir, int indentation = 0)
 {
@@ -36,8 +35,10 @@ void printFolderContents(Folder dir, int indentation = 0)
   std::vector<UFile> files = dir.getFiles();
 
   // Print directories
-  for (Folder subdir : directories) {
-    for (int i = 0; i < indentation; i++) {
+  for (Folder subdir : directories)
+  {
+    for (int i = 0; i < indentation; i++)
+    {
       Serial.print("  ");
     }
     Serial.print("[D] ");
@@ -46,8 +47,10 @@ void printFolderContents(Folder dir, int indentation = 0)
   }
 
   // Print files
-  for (UFile file : files) {
-    for (int i = 0; i < indentation; i++) {
+  for (UFile file : files)
+  {
+    for (int i = 0; i < indentation; i++)
+    {
       Serial.print("  ");
     }
     Serial.print("[F] ");
@@ -55,11 +58,11 @@ void printFolderContents(Folder dir, int indentation = 0)
   }
 }
 
-
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   // toggle this to enable debugging output
   Arduino_UnifiedStorage::debuggingModeEnabled = false;
@@ -68,16 +71,22 @@ void setup() {
   internalStorage = InternalStorage();
 
   // Mount the USB storage
-  if(usbStorage.begin()){
+  if (usbStorage.begin())
+  {
     Serial.println("USB storage mounted.");
-  } else {
+  }
+  else
+  {
     Serial.println(errno);
   }
 
-  if(internalStorage.begin()){
-      Serial.println("Internal storage mounted.");
-  } else {
-     Serial.println(errno);
+  if (internalStorage.begin())
+  {
+    Serial.println("Internal storage mounted.");
+  }
+  else
+  {
+    Serial.println(errno);
   }
 
   // Create a root directory in the internal storage
@@ -93,31 +102,38 @@ void setup() {
 
   // Copy the file from internal storage to USB storage
   bool success = file.copyTo(usbStorage.getRootFolder(), true);
-  if (success) {
+  if (success)
+  {
     Serial.println("File copied successfully from internal storage to USB storage.");
-  } else {
+  }
+  else
+  {
     Serial.println("Failed to copy file from internal storage to USB storage.");
     Serial.println(getErrno());
   }
 
   // Move the subdirectory from internal storage to USB storage
   success = subdir.moveTo(usbStorage.getRootFolder(), true);
-  if (success) {
+  if (success)
+  {
     Serial.println("Subdirectory moved successfully from internal storage to USB storage.");
-  } else {
+  }
+  else
+  {
     Serial.println("Failed to move subdirectory from internal storage to USB storage.");
     Serial.println(getErrno());
   }
 
   // Print contents of the USB storage
-  //Serial.println("USB storage contents:");
-  //printFolderContents(usbStorage.getRootFolder());
+  // Serial.println("USB storage contents:");
+  // printFolderContents(usbStorage.getRootFolder());
 
   // Print contents of the internal storage
   Serial.println("Internal storage contents:");
   printFolderContents(internalStorage.getRootFolder());
 }
 
-void loop() {
+void loop()
+{
   // Nothing to do here
 }
